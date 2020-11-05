2406d321ccba2bc85dbcbd787e17fd80dffa1012
Charles McCarthy <charles@studybundles.com>
2020-11-05 21:20:52 +0000

    Initial Setup

---
+++ ElectionVote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElectionVote
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
