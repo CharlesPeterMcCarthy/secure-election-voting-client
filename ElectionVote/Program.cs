3b1fefe3bc145024946e5b14681485be8350a85c
Charles McCarthy <charles@studybundles.com>
2020-11-06 21:30:14 +0000

    Create Voter Account

--- ElectionVote/Program.cs
+++ ElectionVote/Program.cs
@@ -1,19 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 using ElectionVote.Services;
 
-namespace ElectionVote
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Making a request!");
+namespace ElectionVote {
+    class MainClass {
+        public static async System.Threading.Tasks.Task Main(string[] args) {
+            Console.WriteLine("Welcome to the Election Voting App!\n");
+            Console.WriteLine("Are you Logging in or Signing up?");
+            Console.WriteLine("Enter 1 for Logging in or 2 for Signing up.\n");
 
-            HttpRequest.Request();
+            //int entryVal = int.Parse(Console.ReadLine());
 
-            String input = Console.ReadLine();
+            //Dictionary<String, String> userDetails = new Dictionary<String, String> {
+            //        { "firstName", "Charlie" },
+            //        { "lastName", "McC" },
+            //        { "email", "chazooo555@gmail.com" },
+            //        { "userType", "VOTER" }
+            //    };
 
-            Console.WriteLine(input);
+            //Console.WriteLine("About to post");
+
+            //HttpRequest.Post("https://sp321m383l.execute-api.eu-west-1.amazonaws.com/dev/user", userDetails);
+
+            //Console.WriteLine("Posted");
+
+            //int ssf = int.Parse(Console.ReadLine());
+
+            //HttpRequest.Request();
+
+            int entryVal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Are you a Voter or Admin?");
+            Console.WriteLine("Enter 1 for Voter or 2 for Admin.\n");
+
+            int userTypeVal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(entryVal);
+            Console.WriteLine(userTypeVal);
+
+            if (entryVal == 1) { // Logging in
+
+            } else if (entryVal == 2) { // Signing up
+                Console.Write("Enter first name: ");
+                String firstName = Console.ReadLine();
+                Console.Write("Enter last name: ");
+                String lastName = Console.ReadLine();
+                Console.Write("Enter email address: ");
+                String email = Console.ReadLine();
+
+                String userType = "VOTER";
+                if (userTypeVal == 2) userType = "ADMIN";
+
+                Dictionary<String, String> userDetails = new Dictionary<String, String> {
+                    { "firstName", firstName },
+                    { "lastName", lastName },
+                    { "email", email },
+                    { "userType", userType }
+                };
+
+                String response = await HttpRequest.Post("https://sp321m383l.execute-api.eu-west-1.amazonaws.com/dev/user", userDetails);
+
+                Console.WriteLine(response);
+            }
+
+
         }
     }
 }
--- ElectionVote/Services/HttpRequest.cs
+++ ElectionVote/Services/HttpRequest.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace ElectionVote.Services {
     public static class HttpRequest { 
         private static readonly HttpClient client = new HttpClient();
 
-
-        public static async void Request() {
+        public static async void Get(String url) {
             HttpResponseMessage response;
 
             try {
                 response = await client.GetAsync(
-                    "https://sp321m383l.execute-api.eu-west-1.amazonaws.com/dev/health"
+                    url
+                    //"https://sp321m383l.execute-api.eu-west-1.amazonaws.com/dev/health"
                     //new StringContent(
                     //    JsonConvert.SerializeObject(
                     //        new Dictionary<string, string> {
@@ -37,6 +38,39 @@ namespace ElectionVote.Services {
             var responseObject = JsonConvert.DeserializeObject<dynamic>(responseString);
 
             Console.WriteLine(responseString);
+        }
+
+        public static async Task<String> Post(String url, Dictionary<string, string> postData) {
+            HttpResponseMessage response;
+
+            try {
+                Console.WriteLine("try");
+                response = await client.PostAsync(
+                    url,
+                    new StringContent(
+                        JsonConvert.SerializeObject(postData),
+                        Encoding.UTF8,
+                        "text/json"
+                    )
+                );
+                Console.WriteLine("after res");
+
+                response.EnsureSuccessStatusCode();
+
+                string responseString = await response.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                Console.WriteLine(responseString);
+                //Console.WriteLine(responseObject["success"]);
+                //Console.WriteLine(responseObject["success"]);
+
+                return responseString;
+            } catch (Exception e) {
+                Console.WriteLine("Exception");
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
         }
     }
 }
