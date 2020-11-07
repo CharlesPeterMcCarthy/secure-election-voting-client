1fdab4bf611060d4739392389408e774f78ddff8
Charles McCarthy <charles@studybundles.com>
2020-11-07 14:46:18 +0000

    Auth Flow Interaction

--- ElectionVote/Program.cs
+++ ElectionVote/Program.cs
@@ -2,46 +2,15 @@
 using System.Collections.Generic;
 using ElectionVote.Services;
 using ElectionVote.Services.Enums;
+using ElectionVote.Services.Interactions;
 using ElectionVote.Services.Models;
 
 namespace ElectionVote {
     class MainClass {
         public static async System.Threading.Tasks.Task Main(string[] args) {
             Console.WriteLine("Welcome to the Election Voting App!\n");
-            Console.WriteLine("Are you Logging in or Signing up?");
-            Console.WriteLine("Enter 1 for Logging in or 2 for Signing up.\n");
 
-            int entryVal = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Are you a Voter or Admin?");
-            //Console.WriteLine("Enter 1 for Voter or 2 for Admin.\n");
-
-            //int userTypeVal = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine(entryVal);
-            //Console.WriteLine(userTypeVal);
-
-            //if (entryVal == 1) { // Logging in
-
-            //} else if (entryVal == 2) { // Signing up
-            //    Console.Write("Enter first name: ");
-            //    String firstName = Console.ReadLine();
-            //    Console.Write("Enter last name: ");
-            //    String lastName = Console.ReadLine();
-            //    Console.Write("Enter email address: ");
-            //    String email = Console.ReadLine();
-
-            //    String userType = "VOTER";
-            //    if (userTypeVal == 2) userType = "ADMIN";
-
-            //    User user = await Auth.SignUp(firstName, lastName, email, userType);
-
-            //    Console.WriteLine(user);
-            //    Console.WriteLine(user.firstName);
-            //}
-
-            User user = await Auth.SignUp("Charles", "mcc", "test@test.com", UserType.VOTER);
-            //User user = await Auth.Login("test@test.com");
+            User user = await AuthFlow.Interact();
 
             if (user != null) {
                 Console.WriteLine(user);
---
+++ ElectionVote/Services/Interactions/AuthFlow.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using ElectionVote.Services.Enums;
+using ElectionVote.Services.Models;
+
+namespace ElectionVote.Services.Interactions {
+    public static class AuthFlow {
+
+        public static async Task<User> Interact() {
+            User user = null;
+            int entryVal = 0;
+            int userTypeVal = 0;
+
+            do {
+                Console.WriteLine("Are you Logging in or Signing up?");
+                Console.WriteLine("Enter 1 for Logging in or 2 for Signing up.\n");
+
+
+                entryVal = int.Parse(Console.ReadLine());
+            } while (entryVal != 1 && entryVal != 2);
+
+            do {
+                Console.WriteLine("Are you a Voter or Admin?");
+                Console.WriteLine("Enter 1 for Voter or 2 for Admin.\n");
+
+                userTypeVal = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(entryVal);
+                Console.WriteLine(userTypeVal);
+
+                if (entryVal == 1) { // Logging in
+                    Console.WriteLine("Enter your Email Address: ");
+                    String email = Console.ReadLine();
+
+                    user = await Auth.Login(email);
+                } else if (entryVal == 2) { // Signing up
+                    Console.Write("Enter first name: ");
+                    String firstName = Console.ReadLine();
+                    Console.Write("Enter last name: ");
+                    String lastName = Console.ReadLine();
+                    Console.Write("Enter email address: ");
+                    String email = Console.ReadLine();
+
+                    UserType userType = UserType.VOTER;
+                    if (userTypeVal == 2) userType = UserType.ADMIN;
+
+                    user = await Auth.SignUp(firstName, lastName, email, userType);
+                } else {
+                    Console.WriteLine("You entered an invalid value");
+                }
+
+            } while (userTypeVal != 1 && userTypeVal != 2);
+
+            return user;
+        }
+
+    }
+}
