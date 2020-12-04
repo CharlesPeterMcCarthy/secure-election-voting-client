062b33d7b6dabbc6202f4ee59efcd4bf54fa1366
Charles McCarthy <charles@studybundles.com>
2020-12-04 19:58:46 +0000

    Election Registration Flow: Dynamic Navigation Options

--- ElectionVote/Services/Interactions/Options/RegistrationOptionsFlow.cs
+++ ElectionVote/Services/Interactions/Options/RegistrationOptionsFlow.cs
@@ -1,46 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using ElectionVote.Services.Interactions.Tasks.Registrations;
+using ElectionVote.Services.Models;
 
 namespace ElectionVote.Services.Interactions.Options {
     public static class RegistrationOptionsFlow {
 
+        private static List<NavigationOption> NavigationOptions = new List<NavigationOption>() {
+            new NavigationOption() {
+                Name = "Register for an Election",
+                Action = RegisterForElectionFlow.Interact,
+                IsAccessibleToAll = false,
+                IsVoterOnly = true
+            },
+            new NavigationOption() {
+                Name = "Remove a Registration",
+                Action = RemoveRegistrationFlow.Interact,
+                IsAccessibleToAll = false,
+                IsVoterOnly = true
+            }
+        };
+
         public static async Task Interact() {
-            int optionVal = 0;
+            int selectedNavOption = 0;
 
             Console.WriteLine("------ Election Registrations ------");
 
             do {
-                PrintOptions();
+                CommonFlow.PrintNavigationOptions(NavigationOptions, "election registration");
 
                 try {
-                    optionVal = int.Parse(Console.ReadLine());
+                    selectedNavOption = int.Parse(Console.ReadLine());
                 } catch (FormatException) {
                     CommonFlow.InvalidValueWarning();
                     continue;
                 }
-            } while (optionVal < 1 || optionVal > 2);
+
+                if (selectedNavOption < 1 || selectedNavOption > NavigationOptions.Count) CommonFlow.InvalidValueWarning();
+            } while (selectedNavOption < 1 || selectedNavOption > NavigationOptions.Count);
 
             Console.Clear();
 
-            switch (optionVal) {
-                case 1: // Register for an Election
-                    await RegisterForElectionFlow.Interact();
-                    break;
-                case 2: // Remove a Registration
-                    await RemoveRegistrationFlow.Interact();
-                    break;
-                default:
-                    CommonFlow.InvalidValueWarning();
-                    break;
-            }
-        }
-
-        private static void PrintOptions() {
-            Console.WriteLine("What election registration option would you like?");
-            Console.WriteLine("Options:");
-            Console.WriteLine("1) Register for an Election");
-            Console.WriteLine("2) Remove a Registration");
+            await NavigationOptions[selectedNavOption - 1].Action();
         }
 
     }
